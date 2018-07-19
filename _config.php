c92211d2ad9d14c5e3826b8fb1c21f12098c6d7c
Nicolaas <github@sunnysideup.co.nz>
2018-07-19 17:00:50 +1200

    Update _config.php

--- _config.php
+++ _config.php
@@ -1,3 +1,6 @@
 <?php
 
+// see: https://dev.mysql.com/doc/refman/8.0/en/sql-mode.html#sqlmode_ansi
+// and: https://dev.mysql.com/doc/refman/5.6/en/sql-mode.html#sqlmode_only_full_group_by
+
 SilverStripe\ORM\DB::query("SET SESSION sql_mode='REAL_AS_FLOAT,PIPES_AS_CONCAT,ANSI_QUOTES,IGNORE_SPACE';");
