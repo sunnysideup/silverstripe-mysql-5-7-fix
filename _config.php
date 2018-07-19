d9dcdd7583c0f0df2b79e63d725c5502f4418216
Nicolaas <github@sunnysideup.co.nz>
2018-07-19 19:00:57 +1200

    Update _config.php

--- _config.php
+++ _config.php
@@ -2,5 +2,8 @@
 
 // see: https://dev.mysql.com/doc/refman/8.0/en/sql-mode.html#sqlmode_ansi
 // and: https://dev.mysql.com/doc/refman/5.6/en/sql-mode.html#sqlmode_only_full_group_by
+// we take the standard ANSI settings, 
+// and apply all of them individually, 
+// EXCEPT only_full_group_by as this is causing issues in mysql 5.7+
 
 SilverStripe\ORM\DB::query("SET SESSION sql_mode='REAL_AS_FLOAT,PIPES_AS_CONCAT,ANSI_QUOTES,IGNORE_SPACE';");
